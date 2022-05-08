61b510e18dc023e8a244e57dc0c35f6b446c959e
preben Wulff <prebenox@gmail.com>
2022-05-08 18:03:48 +0200

    nytt innhold, influencere etc

--- components/elements/Preloader.js
+++ components/elements/Preloader.js
@@ -4,10 +4,10 @@ const Preloader = () => {
     return (
         <>
             <div id="preloader-active  ">
-            <div className="preloader flex-1 content-center">
-                <div className="logo absolute inset-y-2/4 jump flex justify-center items-center flex-col ">
+            <div className="flex-1 justify-center items-center content-center pt-40  ">
+                <div className=" inset-y-2/4 jump flex justify-center items-center flex-col ">
                     <img src="/favicon.ico" className='flex items-center' alt="Aug" />
-                    <h1 className="text-lg font-semibold">AUGTRACK</h1>
+                    <h1 className="text-lg font-semibold">MEDIATRAFIKK</h1>
                 </div>
             </div>
         </div>
